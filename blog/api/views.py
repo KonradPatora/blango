324f31b1799a15d40616b762df441583599fa67c
Konrad Patora <konrad.patora@t-mobile.pl>
2022-06-07 20:07:32 +0000

    Finish viewsets and routers

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,11 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 #from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
-from blog.models import Post
+from blog.api.serializers import (
+    PostSerializer,
+    UserSerializer,
+    PostDetailSerializer,
+    TagSerializer,
+)
+from blog.models import Post, Tag
 from blango_auth.models import User
-
 
 
 class UserDetail(generics.RetrieveAPIView):
@@ -14,12 +20,24 @@ class UserDetail(generics.RetrieveAPIView):
     serializer_class = UserSerializer
 
 
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostDetailSerializer
+
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(methods=["get"], detail=True, name="Posts with the Tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        post_serializer = PostSerializer(
+            tag.posts, many=True, context={"request": request}
+        )
+        return Response(post_serializer.data)
